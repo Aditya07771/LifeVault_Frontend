d136cd338062727eda5444693da66cc2c72bf88f
Aditya07771 <adityanishad753@gmail.com>
2026-01-26 06:48:52 +0530

    Fix @ alias for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@
 // }));
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
